d3097fa9e5dea7e87882293fd3e690193ced562d
Realmz1 <zulorule@gmail.com>
2025-10-04 13:27:14 -0700

    index js fix

--- database/index.js
+++ database/index.js
@@ -1,12 +1,10 @@
 const { Pool } = require("pg")
 require("dotenv").config()
 
-const connectionString = process.env.DATABASE_URL
-
 const pool = new Pool({
-  connectionString,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // Required for Render SSL
+    rejectUnauthorized: false, // required for Render connections
   },
 })
 
