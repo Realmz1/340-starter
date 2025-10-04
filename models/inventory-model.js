41edfe95013614ee5680fa39c5fe1dd79ba18ae9
Realmz1 <zulorule@gmail.com>
2025-10-04 12:39:36 -0700

    fix inventory bug

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,6 +1,34 @@
 // models/inventory-model.js
 const pool = require("../database/")
 
+/* ***************************
+ *  Get all classifications
+ * ************************** */
+async function getClassifications() {
+  try {
+    const data = await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+    return data
+  } catch (error) {
+    throw error
+  }
+}
+
+/* ***************************
+ *  Get vehicles by classification ID
+ * ************************** */
+async function getVehicleByClassificationId(classificationId) {
+  try {
+    const sql = "SELECT * FROM public.inventory WHERE classification_id = $1"
+    const data = await pool.query(sql, [classificationId])
+    return data.rows
+  } catch (error) {
+    throw error
+  }
+}
+
+/* ***************************
+ *  Get a single vehicle by inventory ID
+ * ************************** */
 async function getVehicleById(invId) {
   try {
     const sql = "SELECT * FROM public.inventory WHERE inv_id = $1"
@@ -11,13 +39,11 @@ async function getVehicleById(invId) {
   }
 }
 
-async function getClassifications() {
-  try {
-    const data = await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
-    return data
-  } catch (error) {
-    throw error
-  }
+/* ***************************
+ *  Module exports
+ * ************************** */
+module.exports = {
+  getClassifications,
+  getVehicleByClassificationId,
+  getVehicleById
 }
-
-module.exports = { getClassifications, getVehicleByClassificationId, getVehicleById }
