34f61bba21d02a84ced81c0f01caf8168a2b7f4d
Realmz1 <zulorule@gmail.com>
2025-10-04 12:45:53 -0700

    fixed incontroller file

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,31 +1,82 @@
 // controllers/invController.js
 const invModel = require("../models/inventory-model")
-const utilities = require("../utilities/")
-
-// existing exports …
+const utilities = require("../utilities")
 
 /* ***************************
- *  Deliver vehicle detail view
+ *  Build inventory by classification view
  * ************************** */
-async function buildByInvId(req, res, next) {
+async function buildByClassificationId(req, res, next) {
   try {
-    const invId = parseInt(req.params.invId)
-    const vehicleData = await invModel.getVehicleById(invId)
+    const classificationId = parseInt(req.params.classificationId)
+    const data = await invModel.getVehicleByClassificationId(classificationId)
+    const className = data.length > 0 ? data[0].classification_name : "Vehicles"
+    const grid = await buildClassificationGrid(data)
 
-    if (!vehicleData) {
-      // if nothing returned, let error middleware handle
-      return next(new Error("Vehicle not found"))
-    }
-
-    const html = utilities.buildDetailView(vehicleData)
-    res.render("inventory/detail", {
-      title: `${vehicleData.inv_make} ${vehicleData.inv_model}`,
+    res.render("inventory/classification", {
+      title: `${className} Vehicles`,
       nav: await utilities.getNav(),
-      content: html
+      grid
     })
   } catch (error) {
     next(error)
   }
 }
 
+/* ***************************
+ *  Build vehicle detail view
+ * ************************** */
+async function buildByInvId(req, res, next) {
+  try {
+    const invId = parseInt(req.params.invId)
+    const vehicle = await invModel.getVehicleById(invId)
+
+    if (!vehicle) {
+      return res.status(404).render("errors/error", {
+        title: "Vehicle Not Found",
+        message: "Sorry, that vehicle could not be found.",
+        nav: await utilities.getNav()
+      })
+    }
+
+    const detailHTML = utilities.buildDetailView(vehicle)
+    res.render("inventory/detail", {
+      title: `${vehicle.inv_make} ${vehicle.inv_model}`,
+      nav: await utilities.getNav(),
+      content: detailHTML
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+/* ***************************
+ *  Build HTML grid for classification listings
+ * ************************** */
+async function buildClassificationGrid(data) {
+  if (!data || data.length === 0) {
+    return "<p class='notice'>No vehicles found for this classification.</p>"
+  }
+
+  let grid = '<ul id="inv-display">'
+  data.forEach((vehicle) => {
+    grid += `
+      <li>
+        <a href="/inv/detail/${vehicle.inv_id}" title="View ${vehicle.inv_make} ${vehicle.inv_model} details">
+          <img src="${vehicle.inv_thumbnail}" alt="Image of ${vehicle.inv_make} ${vehicle.inv_model}">
+        </a>
+        <div class="namePrice">
+          <h2>
+            <a href="/inv/detail/${vehicle.inv_id}">${vehicle.inv_make} ${vehicle.inv_model}</a>
+          </h2>
+          <span>$${new Intl.NumberFormat("en-US").format(vehicle.inv_price)}</span>
+        </div>
+      </li>`
+  })
+  grid += "</ul>"
+  return grid
+}
+
+/* ***************************
+ *  Exports
+ * ************************** */
 module.exports = { buildByClassificationId, buildByInvId }
